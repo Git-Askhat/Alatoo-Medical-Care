46b0567699565e9269033a51a910a0d4c3d4b37b
Askhat Shailoobekov <askat.shailoobekovich@mail.ru>
2022-01-07 14:07:30 +0600

    Added SQlite

--- Upcoming.Designer.cs
+++ Upcoming.Designer.cs
@@ -84,9 +84,9 @@
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(605, 370);
+            this.button5.Location = new System.Drawing.Point(613, 378);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(120, 120);
+            this.button5.Size = new System.Drawing.Size(109, 103);
             this.button5.TabIndex = 1;
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
